1e0e468a46b9c8c73e5e4ff80a20c0db278fd3bd
Tharson <tharson.s14@gmail.com>
2023-09-19 15:54:43 -0400

    Created inital git repositry, adding in Week 1 adn 2 of Powershell scripts

---
+++ NOtes/Week2.ps1
@@ -0,0 +1,19 @@
+﻿Get-Process
+# No help included, must update help system
+Update-Help -Module * -Force
+
+
+# E.g. Help Command
+Get-Help {Command} {-Online} # will pull up the details of command online
+Get-Help Get-Printer -Online
+
+# Command Path 
+Get-ChildItem -Path "C:\Windows" -Filter *.txt
+Get-ChildItem -Path "C:\Windows" -Include *.txt
+
+<#> Syntax
+# -Parameter <> is Argument 
+[Param] is Positional 
+[Param Arg] is optional 
+[] accepts multiple values 
+ <#>
---
+++ NOtes/Week3.ps1
@@ -0,0 +1,24 @@
+﻿<# Pipe ( | )  will put object into another command
+#>
+# Recurse will go into subfolders
+Get-ChildItem C:\Windows -Recurse # Recurse will go into subfolders
+
+# Get Service, select the Objects and only show its Status and NAme attribute
+Get-Service | Select-Object status, name
+
+# Sort it by status, and Out-Host will display to screen, -Paging will display it by pages
+Get-Service | Select-Object status, name | Sort-Object status | Out-Host -Paging
+
+# Where OBject filter
+Get-Service | Where-Object {$_.DependentServices -ne $null}
+    # Get Service, where the Object 's DependentServices are NOT EQUAL (ne) to empty (null)
+
+
+Get-Service | Where-Object {$_.Name -eq "bits"}
+
+Get-Service | Where-Object {$_.Name -match "^B.*"}
+
+
+# Get Services where WorkingSet is greater than 250 mb and name matches anything that starts with a p
+Get-Process | Where-Object {$_.WorkingSet -gt 250MB -and $_.Name -match "^p.*"}
+
