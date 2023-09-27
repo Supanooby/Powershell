9e05e800f7edd1102c92eb4f00cc866d3f76a32b
Tharson <tharson.s14@gmail.com>
2023-09-27 17:53:40 -0400

    Added notes to Week4content, learned about And/Or operators, and Variables

--- NOtes/Week4AddContent.ps1
+++ NOtes/Week4AddContent.ps1
@@ -21,6 +21,8 @@ help Get-Service -Full
 # "[1-9]" represents any number between 1 - 9, "*[a-c].??d" give a string that has any number of characters starting with a-d.??d
 
 # Compare / Filter 
+# eq used for property exact
+# match used for reg expression
 
 #List all services that has "status stopped"
 Get-Service | Where-Object {$_.Status -eq "stopped"}
@@ -31,3 +33,50 @@ Get-Service | Where-Object {$_.Name -match "C[a-e]"}
 # Matxh exact number of letters
 Get-Service | Where-Object {$_.DisplayName -eq "B?????????e"}
 
+#And / Or
+# Find anything that starts with (C and ends in anything) and has working set greater than 100MB
+Get-Process | Where-Object {$_.Name -match "^c.*" -and $_.ws -gt 100MB}
+# Or
+Get-Process | Where-Object {$_.Name -match "^c.*" -or $_.ws -gt 100MB}
+# Put it in a text file
+Get-Process | Where-Object {$_.Name -match "^c.*" -or $_.ws -gt 100MB} | ForEach-Object {Add-Content -Path text1.txt -value "$_"}
+Get-Process | Where-Object {$_.Name -match "^c.*" -or $_.ws -gt 100MB} | ForEach-Object {Add-Content -Path text1.txt -value $_}
+
+
+
+
+# Get-Member
+Get-Service | Get-Member 
+
+
+
+# Sort Objects
+# Sort Ivhexts by ws property
+Get-Process | Sort-Object -Property ws
+# Sort Objets by ws property decending order
+Get-Process | Sort-Object -Property ws -Descending
+# Selecxt properties and create custom proeprty colum
+Get-Service | Select-Object -Property name @{n='Name of Service';e='($_.Name)'}
+
+
+
+
+
+# Stop-Process with variable
+$vb4 = Get-Process -Name "notepad"
+Stop-Process -InputObject $vb4
+# Stop Porcess will not accept Name , it needs process
+Stop-Process -InputObject $vb4.Name
+
+
+
+
+# Delcare Variables
+$Varaible = "MyOwnVaraible"
+# -InputObject will make asisgn object to VB
+$vb = Get-Process -InputObject
+
+$vb = Get-Process -Name *note*
+$vb
+
+$vb3 = Get-Process -Name "Teams"
