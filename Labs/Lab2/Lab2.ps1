ee94073a364714a54033fab79de88a439e819145
Tharson <tharson.s14@gmail.com>
2023-10-13 11:53:20 -0400

    STarted working on Lab 2 (Group Lab) on Powershell

---
+++ Labs/Lab2/Lab2.ps1
@@ -0,0 +1,6 @@
+﻿$compIp = Read-Host "Please provide ip address"
+$compDNS = Read-Host "dns"
+$compName = Read-Host "name"
+
+Add-WindowsFeature Web-IE-Enhanced-Security-Framework
+$AdminIE = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
